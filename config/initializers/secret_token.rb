104efcadd0727b4e26f60d3c626c7b57864100e3
Seb <emartine@bates.edu>
2014-05-05 14:00:57 -0400

    home

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gigbooker::Application.config.secret_key_base = 'f34028315f6c8dc481bd345f1012b851d7e73aec6c655948d41cfe45088bbc2a74ab9c5be4ae3f6852ec7fab33fe3a09a8a3294c19bfc0cfad11b2ede8d48410'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gigbooker::Application.config.session_store :cookie_store, key: '_gigbooker_session'
